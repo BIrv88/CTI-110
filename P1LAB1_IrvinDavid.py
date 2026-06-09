24687de6969ab3bc981b6b3999b1f824a7f9a450
BIrv88 <irvind8662@student.faytechcc.edu>
2026-06-09 11:50:35 -0400

    First python lab
    Taking first and last name and making a welcome message.

---
+++ P1LAB1_IrvinDavid.py
@@ -0,0 +1,8 @@
+# David Irvin
+# 06/09/2026
+# P1LAB1
+# This program will ask the user for their first and last name and join that to a welcome message.
+
+first_name = input("Enter your first name: ")
+last_name = input("Enter your last name: ")
+print("Hello, ",first_name,last_name + "!","welcome to CTI-110!")
